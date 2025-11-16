83e9533bccf45b514d8aee958e74097d2ed64190
Enochteo <enochowoade@gmail.com>
2025-11-16 16:25:31 -0600

    add api endpoints for crud

---
+++ server/routes/services.js
@@ -0,0 +1,18 @@
+import express from "express";
+import serviceControllers from "../controllers/services.js";
+
+const router = express.Router();
+
+router.get("/", serviceControllers.getUsers);
+
+router.post("/", serviceControllers.createUser);
+
+router.post("/requests", serviceControllers.createRequest);
+
+router.get("/requests", serviceControllers.getRequests);
+
+router.patch("/requests/:Id", serviceControllers.updateRequestStudent);
+
+router.delete("/requests/:Id", serviceControllers.deleteRequest);
+
+export default router;
